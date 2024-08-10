89bb0de5973622b79acd1226ae49da4a8492acef
matveygrevtsov <matveygrevcov@gmail.com>
2024-08-10 13:45:40 +0300

    add-module-federation-plugin

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,17 @@
 import Title from "antd/es/typography/Title";
-import React from "react";
+import React, { FC, Suspense, lazy } from "react";
+
+// @ts-ignore
+const RemoteCart = lazy<FC>(() => import("cart/Cart"));
 
 export const CartPage = () => {
-  return <Title>Корзина</Title>;
+  return (
+    <>
+      <Title>Корзина</Title>
+
+      <Suspense>
+        <RemoteCart />
+      </Suspense>
+    </>
+  );
 };
