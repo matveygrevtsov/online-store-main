20b51f538b803d574635a982166f7727356d3352
matveygrevtsov <matveygrevcov@gmail.com>
2024-08-19 10:56:33 +0300

    fix header

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Footer as AntdFooter } from "antd/es/layout/layout";
+
+export const Footer = () => {
+  return (
+    <AntdFooter style={{ textAlign: "center" }}>
+      ONLINE-SHOP ©{new Date().getFullYear()}
+    </AntdFooter>
+  );
+};
