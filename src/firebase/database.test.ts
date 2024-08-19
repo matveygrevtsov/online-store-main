e95ac39512bf379b458cce2e1d675a7b7819c5a4
matveygrevtsov <matveygrevcov@gmail.com>
2024-08-19 09:11:02 +0300

    add jest

---
+++ src/firebase/database.test.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+import { Product, ProductScheme } from "../types";
+import { database } from "./index";
+
+describe("Тестируем подключение к базе данных", () => {
+  it("Проверяем метод для получения списка товаров", async () => {
+    const products = await database.get<Product[]>("products");
+    expect(products).not.toBe(undefined);
+    expect(z.array(ProductScheme).safeParse(products).success).toBe(true);
+  });
+});
