8ba8724e204379c7d0e6a92485ecacc4d2842bc9
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2024-08-25 18:33:48 +0300

    add css modules (#14)
    * add css modules
    
    * add css modules
    
    * add css modules
    
    ---------
    
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import { ProductsList } from './components/ProductList/ProductsList';
 import Title from 'antd/es/typography/Title';
 
+import * as styles from './styles.module.css';
+
 export const MainPage = () => {
   return (
     <>
       <Title>Каталог</Title>
+      <span className={styles.text}>TEXT</span>
       <ProductsList />
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
-const deps = require("./package.json").dependencies;
-const ExternalTemplateRemotesPlugin = require("external-remotes-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require('./package.json').dependencies;
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 
-const OUTPUT_FOLDER_NAME = path.resolve(__dirname, "dist"); // Папка, куда всё заливаться сбилженный проект.
+const OUTPUT_FOLDER_NAME = path.resolve(__dirname, 'dist'); // Папка, куда всё заливаться сбилженный проект.
 
-const headerUrl = process.env.HEADER_URL
-  ? process.env.HEADER_URL
-  : "http://localhost:3001";
+const headerUrl = process.env.HEADER_URL ? process.env.HEADER_URL : 'http://localhost:3001';
 
-const cartUrl = process.env.CART_URL
-  ? process.env.CART_URL
-  : "http://localhost:3002";
+const cartUrl = process.env.CART_URL ? process.env.CART_URL : 'http://localhost:3002';
 
 const moduleFederationConfig = {
-  name: "main",
+  name: 'main',
   remotes: {
     header: `header@${headerUrl}/remoteEntry.js`, // headerUrl - это домен, на котором "живёт" хедер; header - это name, который мы задали в ремоуте в moduleFederationConfig.
     cart: `cart@${cartUrl}/remoteEntry.js`, // cartUrl - это домен, на котором "живёт" хедер; cart - это name, который мы задали в ремоуте в moduleFederationConfig.
   },
   exposes: {
-    "./ProductCounter": "./src/components/ProductCounter/ProductCounter",
+    './ProductCounter': './src/components/ProductCounter/ProductCounter',
   },
   // Библиотеки, которые мы собираемся шерить.
   shared: {
@@ -31,14 +27,14 @@ const moduleFederationConfig = {
       requiredVersion: deps.react,
       eager: true,
     },
-    "react-dom": {
+    'react-dom': {
       singleton: true,
-      requiredVersion: deps["react-dom"],
+      requiredVersion: deps['react-dom'],
       eager: true,
     },
-    "react-router-dom": {
+    'react-router-dom': {
       singleton: true,
-      requiredVersion: deps["react-router-dom"],
+      requiredVersion: deps['react-router-dom'],
       eager: true,
     },
     antd: {
@@ -50,40 +46,46 @@ const moduleFederationConfig = {
 };
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
+  mode: 'development',
+  entry: './src/index.tsx',
   output: {
     path: OUTPUT_FOLDER_NAME,
-    filename: "bundle.js",
+    filename: 'bundle.js',
   }, // выходной файл
   resolve: {
-    extensions: [".tsx", ".ts", ".js", "jsx"],
+    extensions: ['.tsx', '.ts', '.js', 'jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
-        use: "ts-loader",
+        use: 'ts-loader',
       },
       {
-        test: /\.(css)$/,
+        test: /\.css$/,
         use: [
-          "style-loader",
+          'style-loader',
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
               modules: true,
             },
           },
         ],
+        include: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /\.module\.css$/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
     new ModuleFederationPlugin(moduleFederationConfig),
     new ExternalTemplateRemotesPlugin(), // Таким образом мы даём понять, что наше приложение - хостовое.
